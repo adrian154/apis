40b93882a8ebfdbae4a974a04632472879015ae6
adrian154 <94568a@gmail.com>
2021-09-23 22:51:04 -0700

    add root redirect

--- routes/root.js
+++ routes/root.js
@@ -1,3 +1,3 @@
 module.exports = (req, res) => {
-    res.send('docs are pending, go check out the source at <a href="https://github.com/adrian154/apis/">https://github.com/adrian154/apis/</a>');
+    res.redirect("https://github.com/adrian154/apis/");
 };
